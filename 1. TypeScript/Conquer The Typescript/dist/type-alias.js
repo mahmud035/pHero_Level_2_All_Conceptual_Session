8d6078c5c7e6cd01999088ba59255d39725f8180
mahmud035 <mahamudulhasan4148@gmail.com>
2023-05-07 21:33:18 +0600

    type-alias

---
+++ 1. TypeScript/Conquer The Typescript/dist/type-alias.js
@@ -0,0 +1,14 @@
+"use strict";
+const heroIncome = '34';
+const hero4 = {
+    name: 'khan',
+    age: 32,
+};
+const hero5 = {
+    name: 'siam',
+    age: 30,
+};
+const hero6 = {
+    name: 'jalil',
+    age: 35,
+};
