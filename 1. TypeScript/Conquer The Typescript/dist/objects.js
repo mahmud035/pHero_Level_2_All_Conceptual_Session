dbc5e9e7743f24ddf6b5108685bc01104aec5a1f
mahmud035 <mahamudulhasan4148@gmail.com>
2023-05-07 18:21:04 +0600

    object

---
+++ 1. TypeScript/Conquer The Typescript/dist/objects.js
@@ -0,0 +1,9 @@
+"use strict";
+const heroObj = {
+    heroName: 'Khan',
+    age: 32,
+    profession: 'Actor',
+    isMarried: false,
+};
+const { heroName } = heroObj; // destructure
+console.log(heroName);
