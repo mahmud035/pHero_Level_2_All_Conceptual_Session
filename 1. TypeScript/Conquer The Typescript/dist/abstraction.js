fde5c8e441d1cb02dbbea3c1d91bf244adca7f55
mahmud035 <mahamudulhasan4148@gmail.com>
2023-05-09 15:49:23 +0600

    abstraction

---
+++ 1. TypeScript/Conquer The Typescript/dist/abstraction.js
@@ -0,0 +1,14 @@
+"use strict";
+class Car {
+}
+class MyCar extends Car {
+    start() {
+        console.log(`Car starting....`);
+    }
+    stop() {
+        console.log(`Car stopping...`);
+    }
+}
+const car1 = new MyCar();
+car1.start();
+// NOTE: Example hote pare ATM Booth. Unwanted ba o-proyojonio details k user theke hide kore rakha.
