9869743f88245892acf2f03bef06b72e2105fbcd
mahmud035 <mahamudulhasan4148@gmail.com>
2023-05-09 13:33:13 +0600

    mapped types

---
+++ 1. TypeScript/Conquer The Typescript/dist/mapped-types.js
@@ -0,0 +1,13 @@
+"use strict";
+// basic js map()
+const myLuckyNums = [1, 2, 3, 4, 5];
+const myLuckyStrings = myLuckyNums.map((n) => n.toString());
+console.log(myLuckyStrings);
+const friendList = {
+    friend1: 'abc',
+    friend2: 'def',
+    friend3: 'ghi',
+    friend4: 44,
+};
+// friendList.friend1 = 'Fahim';
+console.log(friendList);
